052ad75fa6707041656038738cc8c4d004239c0f
Charles Holbrow <charlesholbrow@gmail.com>
2016-11-27 18:02:07 -0500

    removed argparse. cleaned.

--- run.py
+++ run.py
@@ -4,29 +4,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.fftpack import fft, ifft, fftshift
 
-import argparse
 import logging
 
 
-def int_or_str(text):
-    """Helper function for argument parsing."""
-    try:
-        return int(text)
-    except ValueError:
-        return text
+# input_device (int or str): input device id
+input_device = None
+# output_device (int or str): output device id
+output_device = None
+# channels (int): number of channels. is this input or output?
+channels = 2
+# dtype: audio data type
+dtype = None
+# samplerate (float): sampleing rate. I'm not sure why this is float and not int
+samplerate = None
+# blocksize (int): block size
+blocksize = None
+# latency (float): latency in seconds
+latency = None
 
-parser = argparse.ArgumentParser(description=__doc__)
-parser.add_argument("-i", "--input-device", type=int_or_str,
-                    help="input device ID or substring")
-parser.add_argument("-o", "--output-device", type=int_or_str,
-                    help="output device ID or substring")
-parser.add_argument("-c", "--channels", type=int, default=2,
-                    help="number of channels")
-parser.add_argument("-t", "--dtype", help="audio data type")
-parser.add_argument("-s", "--samplerate", type=float, help="sampling rate")
-parser.add_argument("-b", "--blocksize", type=int, help="block size")
-parser.add_argument("-l", "--latency", type=float, help="latency in seconds")
-args = parser.parse_args()
 
 try:
     import sounddevice as sd
@@ -38,10 +33,10 @@ try:
         cumulated_status |= status
         outdata[:] = indata
 
-    with sd.Stream(device=(args.input_device, args.output_device),
-                   samplerate=args.samplerate, blocksize=args.blocksize,
-                   dtype=args.dtype, latency=args.latency,
-                   channels=args.channels, callback=callback):
+    with sd.Stream(device=(input_device, output_device),
+                   samplerate=samplerate, blocksize=blocksize,
+                   dtype=dtype, latency=latency,
+                   channels=channels, callback=callback):
         print("#" * 80)
         print("press Return to quit")
         print("#" * 80)
@@ -49,7 +44,9 @@ try:
 
     if cumulated_status:
         logging.warning(str(cumulated_status))
+
 except KeyboardInterrupt:
     parser.exit('\nInterrupted by user')
 except Exception as e:
-    parser.exit(type(e).__name__ + ': ' + str(e))
+    print type(e).__name__ + ': ' + str(e)
+    sys.exit()
